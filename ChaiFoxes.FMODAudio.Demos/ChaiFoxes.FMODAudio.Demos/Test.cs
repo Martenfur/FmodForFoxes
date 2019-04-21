3ccaae2a979e3ee3e33b46ab8bcf82407808a93f
gn.fur <patych7@gmail.com>
2019-04-22 00:54:00 +0300

    Moved Demos solution to correct location.

---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChaiFoxes.FMODAudio;
+
+namespace ChaiFoxes.FMODAudio.Demos
+{
+	public static class Test
+	{
+		public static void Play()
+		{
+			AudioMgr.Init("Content/");
+
+			var sound = AudioMgr.LoadStreamedSound("test.mp3");
+
+			var group = AudioMgr.CreateChannelGroup("group");
+
+			AudioMgr.PlaySound(sound, group);
+		}
+	}
+}
