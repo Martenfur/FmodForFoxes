e5846c1795bf2a51cd2f856d62705d51837d04ed
gn.fur <patych7@gmail.com>
2019-04-21 03:02:06 +0300

    Very rough draft of crossplatform demo.

---
+++ ChaiFoxes.FMODAudio/ChaiFoxes.FMODAudio.Android/LibLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ChaiFoxes.FMODAudio
+{
+	public static class LibLoader
+	{
+		public static void Load()
+		{
+			Java.Lang.JavaSystem.LoadLibrary("fmod");
+		}
+	}
+}
