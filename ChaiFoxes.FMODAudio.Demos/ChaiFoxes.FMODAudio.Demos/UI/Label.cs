87f0ac54632f79eeb7e1d850d692bca202362901
Martenfur <patych7@gmail.com>
2020-04-24 02:05:26 +0300

    Added UI label.

---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/UI/Label.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ChaiFoxes.FMODAudio.Demos.UI
+{
+	public class Label : UIElement
+	{
+		public string Text;
+		public Vector2 Position;
+
+		public Label(
+			string text,
+			Vector2 position
+		) : base()
+		{
+			Text = text;
+			Position = position;
+		}
+
+		public override void Update()
+		{
+
+		}
+
+		public override void Draw()
+		{
+			UIController.SpriteBatch.DrawString(
+				Resources.Arial,
+				Text,
+				(Position - Resources.Arial.MeasureString(Text) / 2f).ToPoint().ToVector2(),
+				UIController.Text
+			);
+		}
+	}
+}
