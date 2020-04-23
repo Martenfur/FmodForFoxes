3a225a2d7b09c5588f6b9dd5379ac5d522d173ee
Martenfur <patych7@gmail.com>
2020-04-24 02:04:12 +0300

    Added UI controller.

---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/UI/UIElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaiFoxes.FMODAudio.Demos.UI
+{
+	public abstract class UIElement
+	{
+		public UIElement()
+		{ 
+			UIController.Add(this);	
+		}
+
+		public abstract void Update();
+		public abstract void Draw();
+
+		public void Destroy() =>
+			UIController.Remove(this);
+	}
+}
