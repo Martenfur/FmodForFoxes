d1a82ad9071f0f651c6d1657a5dd4c5a20ef3777
Martenfur <patych7@gmail.com>
2020-04-24 22:09:47 +0300

    Added basic Studio sample.

--- ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/Game1.cs
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/Game1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using ChaiFoxes.FMODAudio.Demos.UI;
+using ChaiFoxes.FMODAudio.Studio;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -12,10 +14,7 @@ namespace ChaiFoxes.FMODAudio.Demos
 	public class Game1 : Game
 	{
 		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
-
-		Texture2D gato;
-
+		
 		float rotation;
 		float rotationSpeed = 0.01f;
 
@@ -31,8 +30,19 @@ namespace ChaiFoxes.FMODAudio.Demos
 				graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 				graphics.SupportedOrientations = DisplayOrientation.Portrait;
 			#endif
+
+			IsMouseVisible = true;
 		}
 
+		Bank masterBank;
+		Bank masterStringBank;
+
+		EventDescription musicDescription;
+		EventDescription sfxDescription;
+
+		EventInstance musicInstance;
+
+		Listener3D l;
 		/// <summary>
 		/// Allows the game to perform any initialization it needs to before starting to run.
 		/// This is where it can query for any required services and load any non-graphic
@@ -42,25 +52,79 @@ namespace ChaiFoxes.FMODAudio.Demos
 		protected override void Initialize()
 		{
 			// All our music files reside in Content directory.
-			FMODManager.Init(FMODMode.Core, "Content");
-		
-			// You can load pretty much any popular audio format.
-			// I'd recommend .ogg for music, tho.
-			var sound = CoreSystem.LoadStreamedSound("test.mp3");
-			sound.Looping = true;
-			//sound.LowPass = 0.1f;
-			//sound.Volume = 2;
-			//sound.Pitch = 2;
-			
-			var channel = sound.Play();
-			
-			Console.WriteLine(channel.Sound == sound);
+			///FMODManager.Init(FMODMode.CoreAndStudio, "Content");
+			UIController.Init(GraphicsDevice);
+			new Button("foxeh", new Vector2(100, 100), new Vector2(64, 64), null, () => Console.WriteLine("kok"));
+			/*
+				l = new Listener3D();
+
+				// You can load pretty much any popular audio format.
+				// I'd recommend .ogg for music, tho.
+				sound = CoreSystem.LoadStreamedSound("test.mp3");
+				sound.Looping = true;
+				//sound.LowPass = 0.1f;
+				//sound.Volume = 2;
+				//sound.Pitch = 2;
+				sound.MinDistance3D = 100;
+				sound.MaxDistance3D = 20000;
+				//sound.3D
+				sound.Mode = FMOD.MODE._3D;
+				sound.Position3D = new Vector3(0, 0, 0);
+
+				var channel = sound.Play();
+
+				Console.WriteLine(sound.MaxDistance3D + " ---- " + channel.MinDistance3D);
+				Console.WriteLine(channel.Mode);
+
+				/*
+				// Add some effects to the sound! :0
+				channel.LowPass = 0.5f;
+				channel.Pitch = 2f;
+				*/
 
 			/*
-			// Add some effects to the sound! :0
-			channel.LowPass = 0.5f;
-			channel.Pitch = 2f;
+			// You would rather place the following in LoadContent() - it's here more for readability.
+			// Here you load any banks that you're using. This could be a music bank, a SFX bank, etc.
+			// The strings bank isn't actually necessary - however if you want to do string lookups, include it.
+			masterBank = StudioSystem.LoadBank("Master.bank");
+			masterStringBank = StudioSystem.LoadBank("Master.strings.bank");
+
+			// Events are split in the code into descriptions and instances.
+			// You may have multiple instances of one event, but the description for it should only be loaded once.
+			// Here is why you want the strings bank loaded, btw. So much more intuitive than Guids.
+			musicDescription = StudioSystem.GetEvent("event:/Music/Demo");
+
+			// Loading an event description by Guid.
+			// FMOD Studio will give you a string like below when you select "Copy Guid". It has to be parsed to be used.
+			FMOD.Studio.Util.parseID("{432d05d4-71d0-49bc-81fc-9c0beeaf217c}", out Guid sfxGuid);
+			sfxDescription = StudioSystem.GetEvent(sfxGuid);
+
+			// There are three ways to load sample data (any non-streamed sounds):
+			// -From a bank. This will keep ALL the bank's data in memory until unloaded.
+			// -From an event description. This will just keep the event's necessary data in memory until unloaded.
+			// -From an event instance. Same as above, except the data will only be in memory while an instance is.
+			// Assess when you need memory loaded and for how long, then choose which method to use properly.
+
+			// The music doesn't need its data pre-loaded, since it'll just play right away, continuously.
+			musicInstance = musicDescription.CreateInstance();
+
+			// Sound effects could be played whenever, so you don't want them constantly loading / unloading.
+			sfxDescription.LoadSampleData();
+
+			musicInstance.Start();
+			//musicInstance.Paused = true;
+			//musicInstance.Volume = 0.5f;
+			//musicInstance.Pitch = 2f;
 			*/
+			// If you have any parameters set within FMOD Studio, you can change them within the code.
+			//musicInstance.SetParameterValue("Local Parameter", 1);
+			//AudioMgr.SetParameterValue("Global Parameter", 0.5f);
+
+			// FMOD Studio has its own instance of the FMOD Core system, so all normal functions will still work.
+			// AKA: DON'T LOAD BOTH AT THE SAME TIME!!! I don't know, nor do I want to know, what would happen.
+			//var sound = AudioMgr.LoadStreamedSound("test.mp3");
+			//sound.Looping = true;
+			//var channel = sound.Play();
 
 			base.Initialize();
 		}
@@ -72,9 +136,9 @@ namespace ChaiFoxes.FMODAudio.Demos
 		protected override void LoadContent()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch(GraphicsDevice);
+			
+			Resources.Load(Content);
 
-			gato = Content.Load<Texture2D>("gato");
 		}
 
 		/// <summary>
@@ -93,6 +157,7 @@ namespace ChaiFoxes.FMODAudio.Demos
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
+			UIController.Update();
 			if (
 				GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
 				|| GamePad.GetState(PlayerIndex.One).Buttons.Start == ButtonState.Pressed
@@ -101,14 +166,18 @@ namespace ChaiFoxes.FMODAudio.Demos
 			{
 				Exit();
 			}
-
-			FMODManager.Update();
+			
+			//FMODManager.Update();
 
 			rotation += rotationSpeed;
 			if (rotation > MathHelper.TwoPi)
 			{
 				rotation -= MathHelper.TwoPi;
 			}
+
+			var mouse = Mouse.GetState().Position.ToVector2() * 100;
+			//l.Position3D = new Vector3(mouse.X, mouse.Y, 0);
+
 
 			base.Update(gameTime);
 		}
@@ -119,7 +188,8 @@ namespace ChaiFoxes.FMODAudio.Demos
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(new Color(187, 163, 255));
+			GraphicsDevice.Clear(UIController.Backgroud);
+			UIController.Draw();
 
 			#if !ANDROID
 				var screenSize = new Vector2(
@@ -132,21 +202,21 @@ namespace ChaiFoxes.FMODAudio.Demos
 				) / 2;
 			#endif
 
-			var scale = Math.Min(screenSize.X, screenSize.Y) / (float)gato.Width;
+			var scale = Math.Min(screenSize.X, screenSize.Y) / (float)Resources.Gato.Width;
 
-			spriteBatch.Begin();
-			spriteBatch.Draw(
-				gato,
+			UIController.SpriteBatch.Begin();
+			UIController.SpriteBatch.Draw(
+				Resources.Gato,
 				screenSize,
 				null,
 				Color.White,
 				rotation,
-				Vector2.One * gato.Width / 2,
+				Vector2.One * Resources.Gato.Width / 2,
 				Vector2.One * scale,
 				SpriteEffects.None,
 				0
 			);
-			spriteBatch.End();
+			UIController.SpriteBatch.End();
 
 			base.Draw(gameTime);
 		}
---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos/Resources.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChaiFoxes.FMODAudio.Demos
+{
+	public static class Resources
+	{
+		public static SpriteFont Arial;
+		
+		public static Texture2D Button;
+		public static Texture2D Gato;
+		
+		public static void Load(ContentManager content)
+		{ 
+			Arial = content.Load<SpriteFont>("Arial");
+
+			Button = content.Load<Texture2D>("Button");
+			Gato = content.Load<Texture2D>("Gato");
+		}
+	}
+}
