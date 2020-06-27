327216c63d2d4b7552749f3e07d36e7d1be99b76
Martenfur <patych7@gmail.com>
2020-06-28 00:55:42 +0300

    Removed old packing scripts.

--- packDebug.bat
+++
@@ -1,2 +0,0 @@
-nuget pack ChaiFoxes.FMODAudio\ChaiFoxes.FMODAudio.Desktop\ChaiFoxes.FMODAudio.Desktop.csproj -Properties Configuration=Release -OutputDirectory Release\
-nuget pack ChaiFoxes.FMODAudio\ChaiFoxes.FMODAudio.Android\ChaiFoxes.FMODAudio.Android.csproj -Properties Configuration=Release -OutputDirectory Release\
