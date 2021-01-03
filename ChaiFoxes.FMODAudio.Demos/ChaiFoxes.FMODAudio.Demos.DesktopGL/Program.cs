05d6446a212d17b451f613c6e4eaf525b6324815
Martenfur <patych7@gmail.com>
2021-01-03 19:03:24 +0300

    Updated DesktopGL demo.

---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.DesktopGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChaiFoxes.FMODAudio.Demos.DesktopGL
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
