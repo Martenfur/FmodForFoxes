289313ac5a34834fa6cf7c35f34e3ddf9c628211
Martenfur <patych7@gmail.com>
2021-01-03 22:16:05 +0300

    Migrated Android demo to mg 3.8.

--- ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.Android/Activity1.cs
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.Android/Activity1.cs
@@ -2,26 +2,33 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Microsoft.Xna.Framework;
 
 namespace ChaiFoxes.FMODAudio.Demos.Android
 {
-	[Activity(Label = "ChaiFoxes.FMODAudio.Demos.Android"
-			, MainLauncher = true
-			, Icon = "@drawable/icon"
-			, Theme = "@style/Theme.Splash"
-			, AlwaysRetainTaskState = true
-			, LaunchMode = LaunchMode.SingleInstance
-			, ScreenOrientation = ScreenOrientation.Portrait
-			, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
-	public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+	[Activity(
+		Label = "@string/app_name",
+		MainLauncher = true,
+		Icon = "@drawable/icon",
+		AlwaysRetainTaskState = true,
+		LaunchMode = LaunchMode.SingleInstance,
+		ScreenOrientation = ScreenOrientation.FullUser,
+		ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize
+	)]
+	public class Activity1 : AndroidGameActivity
 	{
+		private Game1 _game;
+		private View _view;
+
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-			var g = new Game1();
-			SetContentView((View)g.Services.GetService(typeof(View)));
-			g.Run();
+
+			_game = new Game1();
+			_view = _game.Services.GetService(typeof(View)) as View;
+
+			SetContentView(_view);
+			_game.Run();
 		}
 	}
 }
-
--- ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.Android/Properties/AssemblyInfo.cs
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.Android/Properties/AssemblyInfo.cs
@@ -7,21 +7,14 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ChaiFoxes.FMODAudio.Demos.Android")]
-[assembly: AssemblyProduct("ChaiFoxes.FMODAudio.Demos.Android")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("ChaiFoxes.FMODAudio.Demos.Android")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("eef837d7-8ebd-40af-a7e8-be0d96c999aa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +28,3 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
