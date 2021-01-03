6730dceda67b89a68993f969813e15ef001d5de0
Martenfur <patych7@gmail.com>
2021-01-03 21:29:43 +0300

    Convertex WindowsDX sample to mg 3.8.

---
+++ ChaiFoxes.FMODAudio.Demos/ChaiFoxes.FMODAudio.Demos.WindowsDX/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChaiFoxes.FMODAudio.Demos.WindowsDX
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
